245c1e18508d9012247fd21b28c5cfd4a622dc7e
Onyx Cheyne <alternatetarzan@gmail.com>
2022-09-29 13:37:23 -0700

    ERRORS! assignment and changes in My Project

---
+++ My project/Assets/Scritps/BoxClicked.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class BoxClicked : MonoBehaviour
+{
+	GameManager gameManager;
+	CameraMovement cameraScript;
+
+// Start is called before the first frame update
+    void Start()
+    {
+	    gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+	    cameraScript = GameObject.Find("Main Camera").GetComponent<CameraMovement>();
+    }
+
+
+    /// <summary>
+    /// Called every frame while the mouse is over the GUIElement or Collider.
+    /// </summary>
+    void OnMouseOver()
+    {
+        if(Input.GetMouseButtonUp(0)){
+	        Debug.Log(gameObject.name + gameObject.transform.parent.gameObject.name);
+	        ThisBoxClicked();
+            
+        }
+    }
+
+
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+	   
+    }
+	
+	void ThisBoxClicked()
+	{
+		GameObject newPiece = Instantiate(gameManager.prefabXO[Convert.ToInt32(gameManager.xPlayerTurn)]);
+		newPiece.transform.position = gameObject.transform.position;
+		gameManager.xPlayerTurn = !gameManager.xPlayerTurn;
+		
+		cameraScript.FocusOnThing(gameObject.transform.parent.gameObject);
+	}
+    
+    
+    
+}
---
+++ My project/Assets/Scritps/CameraMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+	const int baseCameraSize = 5;
+	public Camera camera;
+	Vector3 goToThisSpot = new Vector3(0,0,-10);
+	
+	
+    // Start is called before the first frame update
+    void Start()
+	{
+    }
+
+    // Update is called once per frame
+    void Update()
+	{
+		transform.position = Vector3.Lerp(camera.transform.position, goToThisSpot, .01f);
+		
+		
+	}
+    
+    
+	public void FocusOnThing(GameObject focusOn)
+	{
+ 
+		goToThisSpot = new Vector3(
+			focusOn.transform.position.x, 
+			focusOn.transform.position.y, 
+			camera.transform.position.z);
+		
+	}
+}
